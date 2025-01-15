06281276c4a2eab7108241dd3f1be657b34d9fb7
Shreshth Agarwal <68282910+shreshthagarwal@users.noreply.github.com>
2025-01-15 19:35:54 +0530

    Update image-controller.js

--- server/controllers/image-controller.js
+++ server/controllers/image-controller.js
@@ -1,7 +1,7 @@
 import grid from "gridfs-stream";
 import mongoose from "mongoose";
 
-const url = "http://localhost:8000";
+const url = "https://travora-backend.vercel.app";
 
 let gfs, gridfsBucket;
 const conn = mongoose.connection;
